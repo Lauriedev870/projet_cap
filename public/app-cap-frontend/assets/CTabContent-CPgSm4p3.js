5fa5111715cb2bf4fc7a6e9c2668ed8dc53bccf0
fadelsew02 <sewadefadel@gmail.com>
2026-02-09 12:05:48 +0100

    fix(progiciel): reparation du base path

--- public/app-cap-frontend/assets/CTabContent-CPgSm4p3.js
+++
@@ -1,1 +0,0 @@
-import{r as o,_ as l,R as m,c as p,P as s}from"./index-BGf7jE4H.js";const a=o.forwardRef((e,r)=>{var{children:t,className:c}=e,n=l(e,["children","className"]);return m.createElement("div",Object.assign({className:p("tab-content",c)},n,{ref:r}),t)});a.propTypes={children:s.node,className:s.string};a.displayName="CTabContent";export{a as C};
